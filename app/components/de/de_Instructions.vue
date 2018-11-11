efed51ba61657cf41ea2fc554df3f0f1f1f3fd08
Jan Philipp Bittner <jan.bittner@student.uni-halle.de>
2018-11-11 22:11:58 +0100

    du zu sie

--- app/components/de/de_Instructions.vue
+++ app/components/de/de_Instructions.vue
@@ -3,13 +3,13 @@
     <h1 class="instructions__title">Anleitung für das <abbr title="Lernstil-Inventar">LSI</abbr></h1>
     <ol class="instructions__list">
       <li class="instructions__item instruction">
-        <span class="instruction__text" v-once>Bewerte die Enden von {{ sentenceCount }} Sätzen:<br> 4 ist die höchste <br>1 die niedrigste Punktzahl</span>
+        <span class="instruction__text" v-once>Bewerten sie die Enden von {{ sentenceCount }} Sätzen:<br> 4 ist die höchste <br>1 die niedrigste Punktzahl</span>
       </li>
       <li class="instructions__item instruction">
         <span class="instruction__text">Zwei Enden können nicht die gleiche Punktzahl haben.</span>
       </li>
       <li class="instructions__item instruction">
-        <span class="instruction__text">Wähle die Antworten möglichst intuitiv.</span>
+        <span class="instruction__text">Wählen sie die Antworten möglichst intuitiv.</span>
       </li>
     </ol>
     <router-link to="/app/de/inventory" tag="a" class="instructions__button button button--primary">Beginnen</router-link>
